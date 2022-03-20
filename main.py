4a6e5c205e8c8dc5a526f6915378f0757556a03c
egi satria dharma y.w <76691272+egipapers321@users.noreply.github.com>
2022-03-20 15:43:36 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,65 +1,33 @@
-# import package requests dan BeautifulSoup
-import requests
-from bs4 import BeautifulSoup
-
-# Request ke website
-page = requests.get("https://www.republika.co.id/")
-
-# Extract konten menjadi objek BeautifulSoup
-obj = BeautifulSoup(page.text,'html.parser');
-
-print('Menampilkan objek html')
-print('=======================')
-print(obj)
-
-print('\nMenampilkan title browser dengan tag')
-print('======================================')
-print(obj.title)
-
-print('\nMenampilkan title browser tanpa tag')
-print('=====================================')
-print(obj.title.text)
-
-print('\nMenampilkan semua tag h2')
-print('=====================================')
-print(obj.find_all('h2'))
-
-print('\nMenampilkan semua teks h2')
-print('=====================================')
-for headline in obj.find_all('h2'):
-    print(headline.text)
-
-print('\nMenampilkan headline berdasarkan div class')
-print('============================================')
-print(obj.find_all('div',class_='bungkus_txt_headline_center'))
-
-print('\nMenampilkan semua teks headline')
-print('===================================')
-for headline in obj.find_all('div',class_='bungkus_txt_headline_center'):
-    print(headline.find('h2').text)
-
-print('\nMenyimpan headline pada file text')
-print('===================================')
-f=open('C:\\scraping\headline.txt','w')
-for headline in obj.find_all('div', class_='bungkus_txt_headline_center'):
-    f.write(headline.find('h2').text)
-    f.write('\n')
-f.close()
-
-import json
+from selenium import webdriver
 from datetime import datetime
-# Deklarasi list kosong
-data=[]
+import json
+import urllib.request
+
+
+PATH = "C:\webdrivers\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.get("https://www.billboard.com/charts/year-end/hot-100-songs/")
+
+songlist = []
 now = datetime.now()
-# Lokasi file json
-f=open('C:\scraping\headline.json','w')
-for publish in obj.find_all('div',class_='conten1'):
-    # append headline ke variable data
-    data.append({"Judul":publish.find('h2').text,
-                 "Kategori":publish.find('a').text,
-                 "Waktu_publish":publish.find('div',class_='date').text,
-                 "Waktu_scraping":now.strftime("%Y-%m-%d %H:%M:%S")})
-# dump list dictionary menjadi json
-jdumps = json.dumps(data, indent=1)
-f.writelines(jdumps)
-f.close()
+i = 1
+while i<=100:
+    for song in driver.find_elements_by_class_name("o-chart-results-list-row-container"):
+        print(song.text.split("\n"))
+        for img in song.find_elements_by_tag_name("img"):
+            print(img.get_attribute("src"))
+            urllib.request.urlretrieve(img.get_attribute("src"), str(i)+".jpg")
+            i = i+1
+            songlist.append(
+                {"No": song.text.split("\n")[0],
+                "Song": song.text.split("\n")[1],
+                "Singer": song.text.split("\n")[2],
+                "Image": img.get_attribute("src"),
+                "ScrappingTime": now.strftime("%d %B %Y %H:%M:%S")
+                }
+            )
+hasil_scraping = open("hasil.json", "w")
+json.dump(songlist, hasil_scraping, indent = 6)
+hasil_scraping.close()
+
+driver.quit()
